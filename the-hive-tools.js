6de175f2f885225c3ff3651aef39299c96e1ea7b
EifelDriver <eifel.driver@googlemail.com>
2019-03-08 07:51:52 +0100

    Extend script

--- the-hive-tools.js
+++ the-hive-tools.js
@@ -12,11 +12,24 @@
     'use strict';
 
     // --- settings ---
+    var js_name                 = 'the-hive-tools';
     var js_version              = '0.0.1';
     var js_debug                = 1;
     var watcher1, watcher2;
 
     // --- config vars ---
+    var cfg                     = {};
+    if (true) {
+
+        var cfg_options = {
+            highlight_friends   : {type: 'bool',    label: 'Freunde hervorheben',       val: 1},
+            color_friend_bg     : {type: 'color',   label: 'Freund on Hintergrund',     val: '#ffff00'},
+            color_friend_fg     : {type: 'color',   label: 'Freund on Vordergrund',     val: '#333333'},
+            friends_list        : {type: 'list',    label: 'Freundesliste',             val: []},
+            version             : {type: 'readonly',label: 'Version',                   val: '?'}
+        };
+
+    }
 
     // --- observer ---
     var sidebar_mo = new MutationObserver( function(mutations) {
@@ -87,6 +100,55 @@
         insertCss(css, css_id);
     }
 
+    // ============= save config ===============
+
+    /**
+     * save config to loacal storage
+     *
+     * @param cfg
+     * @returns {boolean}
+     */
+    function saveConfig(data) {
+        var success = false;
+        if (typeof data == 'object') {
+            localStorage.setItem(js_name, JSON.stringify(data));
+            success = true;
+            _debug('cfg saved');
+        }
+        return success;
+    }
+
+    /**
+     * load config from local storage
+     */
+    function loadConfig() {
+        var data;
+        try {
+            data = JSON.parse(localStorage.getItem(js_name));
+            if (data === null) {
+                throw "cfg loading error";
+            }
+            _debug('cfg loaded');
+            for (var property in cfg_options) {
+                if (!data.hasOwnProperty(property)) {
+                    // set missing property with default value
+                    data[property] = cfg_options[property]['val'];
+                    _debug('add config option : ' + property);
+                }
+            }
+            _debug('cfg verified');
+        }
+        catch(err) {
+            // stored cfg incorrect -> use default values
+            data = {};
+            for (var property in cfg_options) {
+                data[property] = cfg_options[property]['val'];
+            }
+            _debug('cfg loading failed - use default settings');
+        }
+        cfg = data;
+        saveConfig(cfg);
+    }
 
     // ===============  main  ==================
 
@@ -96,9 +158,44 @@
             var sidebar = document.querySelector('.sidebar.boxesSidebarRight .boxContainer');
             if (sidebar) {
                 var box = document.createElement('SECTION');
-                box.className = 'box';
-                box.innerHTML = users.innerHTML;
+                box.id          = 'my-users-online';
+                box.className   = 'box';
+                box.innerHTML   = users.innerHTML;
                 sidebar.prepend(box);
+                makeFriendsSelectable('#my-users-online');
+            }
+        }
+    }
+
+    function makeFriendsSelectable(selector) {
+        var users = document.querySelectorAll(selector + ' a.userLink');
+        users.forEach(
+            function(item) {
+                item.addEventListener('click', showAddFriendDialog, false);
+            }
+        )
+    }
+
+    function showAddFriendDialog(e) {
+        // TODO: add own contextmenu
+        if (e.button == 0) {    // left click
+            e.preventDefault();
+            e.stopPropagation();
+            var elem = e.target;
+            if (elem.tagName.toLowerCase() == 'font') {
+                elem = elem.parentNode;
+            }
+            var user_name = elem.innerText.trim();
+            addFriendToWatchlist(user_name);
+            return false;
+        }
+    }
+    
+    function addFriendToWatchlist(user_name) {
+        if (cfg) {
+            if (cfg.friends_list.indexOf(user_name) == -1) {
+                cfg.friends_list.push(user_name);
+                saveConfig(cfg);
             }
         }
     }
@@ -106,19 +203,8 @@
     // ---  init script ---
     function startScript() {
         _debug('the-hive-tools started');
+        loadConfig();
         addPortletUsersOnline();
-    }
-
-    // ---  window loaded  ---
-    if (false) {
-        /**
-         * exec some tasks after the page has loading finished
-         */
-        function pageLoadFinish(e) {
-            window.removeEventListener('load', pageLoadFinish); // prevent multiple calls
-            window.setTimeout(startScript, 500);
-        }
-        // window.addEventListener('load', pageLoadFinish, false, true); // use bubbling and be cancelable
     }
 
     // --- wait for window loaded event ---
